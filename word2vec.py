41dd3b329b8a27deb6bce56da2541558e3bcaf06
Moseli Motsoehli <u12061019@tuks.co.za>
2018-05-15 14:29:08 +0200

    Changed targets to one hot
    I removed skipgram embeddings for the target words ad instead use one hot encoded vectors. The input to the encoder remains as skipgram embedding

--- word2vec.py
+++ word2vec.py
@@ -17,9 +17,14 @@ import scipy as sc
 import nltk
 from nltk.tokenize import word_tokenize as wt
 from nltk.tokenize import sent_tokenize as st
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 from keras.preprocessing.sequence import pad_sequences
 import logging
 import re
+from collections import Counter
+
 
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
@@ -28,6 +33,9 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
 ########################################################################
 ############################helpers####################################
 #######################################################################
+emb_size_all = 128
+maxcorp=5000
+
 
 def createCorpus(t):
     corpus = []
@@ -51,17 +59,17 @@ def createCorpus(t):
 
 
 def word2vecmodel(corpus):
-    emb_size=128
+    emb_size = emb_size_all
     model_type={"skip_gram":1,"CBOW":0}
-    window=5
+    window=10
     workers=4
-    min_count=3
-    batch_words=75
-    epochs=20
+    min_count=4
+    batch_words=20
+    epochs=25
     #include bigrams
-    bigramer = gs.models.Phrases(corpus)
+    #bigramer = gs.models.Phrases(corpus)
 
-    model=gs.models.Word2Vec(bigramer[corpus],size=emb_size,sg=model_type["skip_gram"],
+    model=gs.models.Word2Vec(corpus,size=emb_size,sg=model_type["skip_gram"],
                              compute_loss=True,window=window,min_count=min_count,workers=workers,
                              batch_words=batch_words)
         
@@ -69,7 +77,41 @@ def word2vecmodel(corpus):
     model.save("%sWord2vec"%modelLocation)
     print('\007')
     return model
+
+
+def summonehot(corpus):
+    allwords=[]
+    annotated={}
+    for sent in corpus:
+        for word in wt(sent):
+            allwords.append(word.lower())
+    print(len(set(allwords)), "unique characters in corpus")
+    #maxcorp=int(input("Enter desired number of vocabulary: "))
+    maxcorp=int(len(set(allwords))/1.1)
+    wordcount = Counter(allwords).most_common(maxcorp)
+    allwords=[]
     
+    for p in wordcount:
+        allwords.append(p[0])  
+        
+    allwords=list(set(allwords))
+    
+    print(len(allwords), "unique characters in corpus after max corpus cut")
+    #integer encode
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(allwords)
+    #one hot
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    #make look up dict
+    for k in range(len(onehot_encoded)): 
+        inverted = cleantext(label_encoder.inverse_transform([argmax(onehot_encoded[k, :])])[0]).strip()
+        annotated[inverted]=onehot_encoded[k]
+    return label_encoder,onehot_encoded,annotated
+
+
+
 def wordvecmatrix(model,data):
     IO_data={"article":[],"summaries":[]}
     i=1
@@ -84,7 +126,8 @@ def wordvecmatrix(model,data):
 
         for word in wt(data["summaries"][k].lower()):
             try:
-                summ.append(model.wv.word_vec(word))
+                summ.append(onehot[word])
+                #summ.append(model.wv.word_vec(word))
             except Exception as e:
                 print(e)
         
@@ -97,21 +140,31 @@ def wordvecmatrix(model,data):
     print('\007')
     return IO_data
 
+def cutoffSequences(data,artLen,sumlen):
+    data2={"article":[],"summaries":[]}
+    for k in range(len(data["article"])):
+        if len(data["article"][k])<artLen or len(data["summaries"][k])<sumlen:
+             #data["article"]=np.delete(data["article"],k,0)
+             #data["article"]=np.delete(data["summaries"],k,0)
+             pass
+        else:
+            data2["article"].append(data["article"][k][:artLen])
+            data2["summaries"].append(data["summaries"][k][:sumlen])
+    return data2
+
 
 def max_len(data):
     lenk=[]
     for k in data:
         lenk.append(len(k))
-    return max(lenk)
+    print("The minimum length is: ",min(lenk))
+    print("The average length is: ",np.average(lenk))
+    print("The max length is: ",max(lenk))
+    return min(lenk),max(lenk)
 
 """reshape vectres for Gensim"""
 def reshape(vec):
-    return np.reshape(vec,(1,128))
-
-"""__find nearest word to given vec__"""
-def getWord(vec):
-    word=model.wv.most_similar(reshape(vec),topn=1)[0]
-    return word
+    return np.reshape(vec,(1,emb_size_all))
 
 def addones(seq):
     return np.insert(seq, [0], [[0],], axis = 0)
@@ -122,24 +175,44 @@ def endseq(seq):
 #######################################################################
 #######################################################################
 
-
 corpus = createCorpus(data)
 
+label_encoder,onehot_encoded,onehot=summonehot(data["summaries"])
+
 model=word2vecmodel(corpus)
+
 model.get_latest_training_loss()
 
 train_data = wordvecmatrix(model,data)
 
-#add end sequence for each article
-train_data["summaries"]=np.array(list(map(endseq,train_data["summaries"])))
-train_data["article"]=np.array(list(map(endseq,train_data["article"])))
+print(len(train_data["article"]), "training articles")
 
-"""__pad sequences__"""
+train_data=cutoffSequences(train_data,300,25)
+
+#seq length stats
+max_len(train_data["article"])
+max_len(train_data["summaries"])
+
+
+train_data["summaries"]=np.array(train_data["summaries"])
+train_data["article"]=np.array(train_data["article"])
+
+
+#add end sequence for each article
+
+#train_data["summaries"]=np.array(list(map(endseq,train_data["summaries"])))
+#train_data["article"]=np.array(list(map(endseq,train_data["article"])))
+
+print("summary length: ",len(train_data["summaries"][0]))
+print("article length: ",len(train_data["article"][0]))
+
+
+"""__pad sequences__
 train_data["article"]=pad_sequences(train_data["article"],maxlen=max_len(train_data["article"]),
           padding='post',dtype=float)
 train_data["summaries"]=pad_sequences(train_data["summaries"],maxlen=max_len(train_data["summaries"]),
           padding='post',dtype=float)
-
+"""
 #add start sequence
 train_data["summaries"]=np.array(list(map(addones,train_data["summaries"])))
 
